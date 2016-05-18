e32487740930ff57350e6598ad02e6a61d595837
ftcgearedup <ftcgearedup@gmail.com>
2016-05-18 00:18:21 -0400

    Sample Code

---
+++ src/org/ftcgearedup/learning/Main.java
@@ -0,0 +1,12 @@
+package org.ftcgearedup.learning;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner reader = new Scanner(System.in);  // Reading from System.in
+        System.out.println("Enter a number: ");
+        int n = reader.nextInt(); // Scans the next token of the input as an int.
+    }
+}
