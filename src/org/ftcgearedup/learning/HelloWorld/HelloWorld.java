c8012a1e856936540f71d031cb36f1e12d7813d0
Daryn Stephenson <deltadaryn@gmail.com>
2016-06-01 19:54:00 -0400

    Hello World!

---
+++ src/org/ftcgearedup/learning/HelloWorld/HelloWorld.java
@@ -0,0 +1,10 @@
+package org.ftcgearedup.learning.HelloWorld;
+
+/**
+ * Created by ftcgearedup on 6/1/2016.
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
