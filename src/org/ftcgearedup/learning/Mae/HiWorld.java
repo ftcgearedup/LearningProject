844c97188e6b668aaff54f9518d605f06af34dbd
MaeF <ftcgearedup@gmail.com>
2016-06-01 19:42:50 -0400

    1st program - Says Hello World and my age

---
+++ src/org/ftcgearedup/learning/Mae/HiWorld.java
@@ -0,0 +1,13 @@
+package org.ftcgearedup.learning.Mae;
+
+/**
+ * Created by Maegan on 6/1/2016.
+ */
+public class HiWorld {
+    public static void main(String[] args) {
+        int age;
+        age = 16;
+        System.out.println("HelloWorld");
+        System.out.println(age);
+    }
+}
