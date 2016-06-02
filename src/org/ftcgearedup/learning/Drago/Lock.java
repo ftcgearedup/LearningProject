458ec2a755d3a2e81d918562868573a84ce5c8f7
drago <ftcgearedup@gmail.com>
2016-06-02 02:42:07 -0400

    Finished 1st version of password "lock" code.

--- src/org/ftcgearedup/learning/Drago/Lock.java
+++ src/org/ftcgearedup/learning/Drago/Lock.java
@@ -1,7 +1,29 @@
 package org.ftcgearedup.learning.Drago;
 
+import java.util.Scanner;
+
 /**
  * Created by Mike on 6/2/2016.
  */
 public class Lock {
+    public static void main(String[] args) {
+        Scanner reader = new Scanner(System.in);
+        system.out.print ("Enter Password Here: ");
+        string x = reader.nextString();
+        if(x = "6347") {
+            system.out.println("Access granted, Master!");
+
+        }
+        else() {
+            for (int i = 1; i<21; i++){
+                system.out.println("NO!");
+            }
+
+
+
+        }
+
+
+    }
+
 }
