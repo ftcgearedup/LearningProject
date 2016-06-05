47c98774f95b3638b4a5437e014eebe140c0d6c6
Daniel T <r0b0tbuilder@users.noreply.github.com>
2016-06-05 00:06:20 -0400

    Implement fib2() - another Fibonacci method

--- src/org/ftcgearedup/learning/dpt/FibonacciChallenge.java
+++ src/org/ftcgearedup/learning/dpt/FibonacciChallenge.java
@@ -6,25 +6,36 @@ package org.ftcgearedup.learning.dpt;
 public class FibonacciChallenge {
     public static void main(String[] args) {
 
-        // fib(n) outputs the nth number in the fibonacci sequence.
+        // The method fib(n) returns the nth number in the fibonacci sequence.
         // The fibonacci sequence is considered to start with 0, not 1.
-
-        // Examples:
-        System.out.println(fib(1));
-        System.out.println(fib(2));
-        System.out.println(fib(5));
-        System.out.println(fib(8));
-
-        System.out.println("=========");
 
         // Loop through the first 10 fibonacci numbers:
         for(int i = 1; i <= 10; i++) {
             System.out.println(fib(i));
         }
+
+        System.out.println("=========");
+
+        // The method fib2(n) prints the first n numbers of the fibonacci sequence.
+
+        // Print the first 10 numbers in the Fibonacci sequence:
+        fib2(10);
     }
 
     private static int fib(int n) {
         return n <= 2 ? n - 1: fib(n - 1) + fib(n - 2);
+    }
+
+    private static void fib2(int n) {
+        int a = 0;
+        int b = 1;
+        int temp;
+        for(; n > 0; n--) {
+            temp = a + b;
+            System.out.println(a);
+            a = b;
+            b = temp;
+        }
     }
 
 }
