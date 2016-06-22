3314332a4e930d8407e11cbe380bc4f0e8ab67c7
drago <ftcgearedup@gmail.com>
2016-06-22 18:21:31 -0400

    Fixed "MikesFibonacciCode"

--- src/org/ftcgearedup/learning/Drago/MikesFibonacciCode.java
+++ src/org/ftcgearedup/learning/Drago/MikesFibonacciCode.java
@@ -7,15 +7,22 @@ public class MikesFibonacciCode {
 
 
     public static void main(String[] args) {
-        int x = 1;  //1st term
+        int x = 0;  //1st term
         int y = 1; // 2nd term
         int z = 1; //actual math done with this command
         System.out.println("Here We Go!");
-        for(int i=1; i<21; i++){
+        System.out.println("0");
+        System.out.println("1");
+        for(int i=0; i<21; i++){
             z = x + y;
             x = y;
             y = z;
             System.out.println(z);
+            try {
+                Thread.sleep(500);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
         }
 
     }
